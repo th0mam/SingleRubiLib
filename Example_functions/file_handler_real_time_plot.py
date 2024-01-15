eb03166ee7877a74686e20fb40b774c00b92d245
th0mam <lamichtom@gmail.com>
2024-01-15 15:10:14 +0100

    Clean up file_handler code. Added file_handler_real_time_plot.py as a next step towards propper real time plotting. Next to do: create init file for the library and start writing functions for the library.

--- Example_functions/file_handler_real_time_plot.py
+++ Example_functions/file_handler_real_time_plot.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 15 12:04:23 2024
+Created on Mon Jan 15 14:59:53 2024
 
 @author: tlamich
 """
-
-
-
 
 import time
 import os
@@ -16,6 +13,7 @@ import glob
 path='C:/Users/tlamich/Data/fake_data/*.txt'
 data=[]
 
+plt.axis()
 while True:
     try:
         if glob.glob(path):
@@ -23,7 +21,8 @@ while True:
             with open(file_path[0],'r') as f:
                 data.append(float(f.read()))
             os.remove(file_path[0])
-            time.sleep(0.1)
+            plt.scatter(len(data),data[-1],c='black')
+            plt.pause(0.1)
         else:
             time.sleep(0.1)
     except KeyboardInterrupt:
